75f49b6c7e323a435985af844d32eb1b85b4d20f
Anton Kindestam <antonki@kth.se>
2017-06-05 21:44:59 +0200

    snesCon_read: Switch to use delay_us

--- snescon-read/snesCon_read.c
+++ snescon-read/snesCon_read.c
@@ -15,70 +15,22 @@ bool snesCon_read_init(const snesCon_read_t * const def)
     return true;
 }
 
-/**
- * delay for 12 us
- *
- * TODO: Currently hardcoded for 8 MHz SYSCLK. Make snazzy variant that can work out how many NOPs
- * are needed, based on g_clock.sysclkFreq
- */
-static void snesCon_delay()
-{
-    __disable_irq();
-    for (volatile uint8_t i = 0; i < 12; ++i)
-    {
-        // 8 NOPs at 8 MHz == 1 us
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-    }
-    __enable_irq();
-}
-
-/**
- * delay for 6 us
- *
- * TODO: Currently hardcoded for 8 MHz SYSCLK. Make snazzy variant that can work out how many NOPs
- * are needed, based on g_clock.sysclkFreq
- */
-static void snesCon_halfDelay()
-{
-    __disable_irq();
-    for (volatile uint8_t i = 0; i < 6; ++i)
-    {
-        // 8 NOPs at 8 MHz == 1 us
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-        NOP();
-    }
-    __enable_irq();
-}
-
 snesCon_btn_t snesCon_read_tick(const snesCon_read_t * const def)
 {
     GPIO_setPin(&def->latch);
-    snesCon_delay();
+    delay_us(12);
     GPIO_resetPin(&def->latch);
 
-    snesCon_halfDelay();
+    delay_us(6);
 
     snesCon_btn_t btn = 0;
     for (uint8_t i = 0; i < 16; ++i)
     {
         GPIO_setPin(&def->clock);
-        snesCon_delay();
+        delay_us(12);
         btn |= (!!GPIO_read(&def->data)) << i;
         GPIO_resetPin(&def->clock);
-        snesCon_delay();
+        delay_us(12);
     }
 
     return btn;

6cc25bbbea8aea52dda0135d6412bbee01d7670e
Anton Kindestam <antonki@kth.se>
2017-06-05 22:46:32 +0200

    snesCon_read: Correct clock behaviour (1)
    Clock is normally high, and goes low.

--- snescon-read/snesCon_read.c
+++ snescon-read/snesCon_read.c
@@ -7,7 +7,7 @@ bool snesCon_read_init(const snesCon_read_t * const def)
     GPIO_setMODE_setCNF(def->clock.port, def->clock.pin, GPIO_MODE_Output_10MHz, GPIO_Output_CNF_GPPushPull);
     GPIO_setMODE_setCNF(def->latch.port, def->latch.pin, GPIO_MODE_Output_10MHz, GPIO_Output_CNF_GPPushPull);
     GPIO_resetPin(&def->latch);
-    GPIO_resetPin(&def->clock);
+    GPIO_setPin(&def->clock);                               // Clock is active low
 
     GPIO_setMODE_setCNF(def->data.port, def->data.pin, GPIO_MODE_Input, GPIO_Input_CNF_PullupPulldown);
     GPIO_resetPin(&def->data); // Set pull-down
@@ -23,13 +23,15 @@ snesCon_btn_t snesCon_read_tick(const snesCon_read_t * const def)
 
     delay_us(6);
 
+    // The zeroth button (B) is driven already on falling latch
+    // The rest on rising clock
     snesCon_btn_t btn = 0;
     for (uint8_t i = 0; i < 16; ++i)
     {
-        GPIO_setPin(&def->clock);
-        delay_us(12);
-        btn |= (!!GPIO_read(&def->data)) << i;
         GPIO_resetPin(&def->clock);
+        delay_us(12);
+        btn |= (!GPIO_read(&def->data)) << i;               // Button pressed when low
+        GPIO_setPin(&def->clock);
         delay_us(12);
     }
 

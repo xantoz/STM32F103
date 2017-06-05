31c63d0f80dfea96e5765165b530f1e5f4050d9e
Anton Kindestam <antonki@kth.se>
2017-06-05 18:00:10 +0200

    systick: compromise to the overflow problem: multiply by sysTickFreq in kHz, instead of in just Hz

--- lib/systick.c
+++ lib/systick.c
@@ -4,9 +4,11 @@
 
 static bool systick_startSysTick_impl(const uint32_t multiplicand, const uint32_t dividend)
 {
+    // Loose some (probably not needed) precision, in exchange for making overflows less likely, by
+    // first converting frequency from Hz to kHz
+    const uint32_t sysTickFreq_kHz = g_clock.sysTickFreq/1000;
     // SysTick RELOAD should be loaded with N-1 to fire every N clock pulses
-    // TODO: consider calculating this at higher precision, or doing the division first. (risk for overflow)
-    const uint32_t reload = (multiplicand*g_clock.sysTickFreq)/dividend - 1;
+    const uint32_t reload = (multiplicand*sysTickFreq_kHz)/dividend - 1;
     // TODO: fail when overflow?
 
     // Fail when reload == 0 or when the value will not fit into the 24-bit wide register
@@ -24,10 +26,10 @@ static bool systick_startSysTick_impl(const uint32_t multiplicand, const uint32_
 
 bool systick_startSysTick_ms(const uint32_t ms)
 {
-    return systick_startSysTick_impl(ms, 1000);
+    return systick_startSysTick_impl(ms, 1);
 }
 
 bool systick_startSysTick_us(const uint32_t us)
 {
-    return systick_startSysTick_impl(us, 1000000);
+    return systick_startSysTick_impl(us, 1000);
 }

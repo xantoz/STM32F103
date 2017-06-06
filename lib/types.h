3f2b9ec7514b2ea63a965a1b7816521766f13f4f
Anton Kindestam <antonki@kth.se>
2017-06-06 10:52:21 +0200

    types: Remove double defintion of size_t

--- lib/types.h
+++ lib/types.h
@@ -4,13 +4,11 @@
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned long uint32_t;
-typedef unsigned long size_t;
+typedef uint32_t size_t;
 
 typedef char int8_t;
 typedef short int16_t;
 typedef long int32_t;
-
-typedef uint32_t size_t;
 
 #ifndef __cplusplus
 #undef bool

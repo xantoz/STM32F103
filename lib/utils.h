3bea4a4f2cfa65628863f05aa3c63581bd2bbfd5
Anton Kindestam <antonki@kth.se>
2017-06-06 10:53:17 +0200

    utils: Implement generic macro DIV_ROUND_CLOSEST that will do integer divisions while rounding sensibly

--- lib/utils.h
+++ lib/utils.h
@@ -45,6 +45,29 @@ extern void delay_us(int32_t us);
  */
 extern void delay_us_int(int32_t us);
 
+#define __DIV_ROUND_CLOSEST_INT(n, d) \
+    ((((n) < 0) ^ ((d) < 0)) ? (((n) - (d)/2)/(d)) : (((n) + (d)/2)/(d)))
+#define __DIV_ROUND_CLOSEST_UINT(n, d) (((n) + (d)/2)/(d))
+#define _DIV_ROUND_CLOSEST_UINT(n,d)                                    \
+    _Generic((d),                                                       \
+             unsigned char:  __DIV_ROUND_CLOSEST_UINT((n),(d)),         \
+             unsigned short: __DIV_ROUND_CLOSEST_UINT((n),(d)),         \
+             unsigned int:   __DIV_ROUND_CLOSEST_UINT((n),(d)),         \
+             unsigned long:  __DIV_ROUND_CLOSEST_UINT((n),(d)),         \
+             signed char:    __DIV_ROUND_CLOSEST_INT((n),(d)),          \
+             signed int:     __DIV_ROUND_CLOSEST_INT((n),(d)),          \
+             signed short:   __DIV_ROUND_CLOSEST_INT((n),(d)),          \
+             signed long:    __DIV_ROUND_CLOSEST_INT((n),(d)))
+#define DIV_ROUND_CLOSEST(n,d)                                     \
+    _Generic((n),                                                  \
+             unsigned char:  _DIV_ROUND_CLOSEST_UINT((n),(d)),     \
+             unsigned short: _DIV_ROUND_CLOSEST_UINT((n),(d)),     \
+             unsigned int:   _DIV_ROUND_CLOSEST_UINT((n),(d)),     \
+             unsigned long:  _DIV_ROUND_CLOSEST_UINT((n),(d)),     \
+             signed char:    __DIV_ROUND_CLOSEST_INT((n),(d)),     \
+             signed short:   __DIV_ROUND_CLOSEST_INT((n),(d)),     \
+             signed int:     __DIV_ROUND_CLOSEST_INT((n),(d)),     \
+             signed long:    __DIV_ROUND_CLOSEST_INT((n),(d)))
 
 #define SET(peripheral, reg, field, value) ((peripheral).(reg) = ((peripheral).(reg) & ~(peripheral##_##reg##_##field)) | peripheral##_##reg##_##field##_##value)
 

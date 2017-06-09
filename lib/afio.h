98f86795d9684ffef012f89dbae936599f18bf0b
Anton Kindestam <antonki@kth.se>
2017-06-09 21:00:02 +0200

    afio.h stub
    TODO: register definitions, at least for MAPR and MAPR2

---
+++ lib/afio.h
@@ -0,0 +1,14 @@
+#ifndef _AFIO_
+#define _AFIO_
+
+/**
+ * @brief Enum defined here as a convenience for use in client functions that
+ * take as an argument the 'default' mapping, or the 'alternate' one.
+ */
+enum AF_Mapping
+{
+    DEFAULT,
+    ALTERNATE
+};
+
+#endif /* _AFIO_ */

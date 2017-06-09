42daea7221ad0360ea48167cd38310a65959d806
Anton Kindestam <antonki@kth.se>
2017-06-09 22:37:34 +0200

    spi: refactor SetupGpio functions even more

--- lib/spi.c
+++ lib/spi.c
@@ -32,54 +32,64 @@ bool spi_getBaudRateDivisorFromMaxFreq(SPI_Struct const * const spi, uint32_t ma
     return false;
 }
 
+typedef struct
+{
+    GPIO_PortPin NSS, SCK, MISO, MOSI;
+} SPI_Pins;
+
 static void spi_setupGpioHelper(enum SPI_OutputMode outputMode,
                                 enum SPI_InputMode inputMode,
-                                GPIO_PortPin const * const NSS,
-                                GPIO_PortPin const * const SCK,
-                                GPIO_PortPin const * const MISO,
-                                GPIO_PortPin const * const MOSI)
+                                SPI_Pins const * const pins)
 {
     uint8_t outputCNF = (outputMode == SPI_PushPull) ? GPIO_Output_CNF_AFPushPull : GPIO_Output_CNF_AFOpenDrain;
     uint8_t inputCNF  = (inputMode == SPI_Floating) ? GPIO_Input_CNF_Floating : GPIO_Input_CNF_PullupPulldown;
 
-    GPIO_setMODE_setCNF(NSS,  GPIO_MODE_Output_50MHz, outputCNF); // NSS
-    GPIO_setMODE_setCNF(SCK,  GPIO_MODE_Output_50MHz, outputCNF); // SCK
-    GPIO_setMODE_setCNF(MOSI, GPIO_MODE_Output_50MHz, outputCNF); // MOSI
+    GPIO_setMODE_setCNF(&pins->NSS,  GPIO_MODE_Output_50MHz, outputCNF); // NSS
+    GPIO_setMODE_setCNF(&pins->SCK,  GPIO_MODE_Output_50MHz, outputCNF); // SCK
+    GPIO_setMODE_setCNF(&pins->MOSI, GPIO_MODE_Output_50MHz, outputCNF); // MOSI
 
     // TODO: consider having a gpio init function which also boxes pullup/pulldown (this would've then been a one-liner)
-    GPIO_setMODE_setCNF(MISO, GPIO_MODE_Input, inputCNF);         // MISO
+    GPIO_setMODE_setCNF(&pins->MISO, GPIO_MODE_Input, inputCNF);         // MISO
     if (inputMode == SPI_PullUp)
-        GPIO_setPin(MISO);
+        GPIO_setPin(&pins->MISO);
     else if (inputMode == SPI_PullDown)
-        GPIO_resetPin(MISO);
+        GPIO_resetPin(&pins->MISO);
 }
 
 // TODO: mold this into a more fully encompassing SPI init function (handle freq, clock phase &c.,
-// do not set up port bits for NSS when not applicable)
+// and do not set up port bits for NSS when not applicable)
 void SPI1_SetupGpio(enum AF_Mapping mapping, enum SPI_OutputMode outputMode, enum SPI_InputMode inputMode)
 {
-    if (mapping == DEFAULT)
-        RESET(AFIO, MAPR, SPI1_REMAP);
-    else if (mapping == ALTERNATE)
+    if (mapping == ALTERNATE)
         SET(AFIO, MAPR, SPI1_REMAP);
     else
-        die("SPI1_MapGpio: Invalid parameter");
+        RESET(AFIO, MAPR, SPI1_REMAP);
 
-    const GPIO_PortPin NSS  = (mapping == DEFAULT) ? (GPIO_PortPin){&GPIOA, 4} : (GPIO_PortPin){&GPIOA, 15};
-    const GPIO_PortPin SCK  = (mapping == DEFAULT) ? (GPIO_PortPin){&GPIOA, 5} : (GPIO_PortPin){&GPIOB, 3};
-    const GPIO_PortPin MISO = (mapping == DEFAULT) ? (GPIO_PortPin){&GPIOA, 6} : (GPIO_PortPin){&GPIOB, 4};
-    const GPIO_PortPin MOSI = (mapping == DEFAULT) ? (GPIO_PortPin){&GPIOA, 7} : (GPIO_PortPin){&GPIOB, 5};
+    static const SPI_Pins SPI1_Default_Mapping = {
+        .NSS  = {&GPIOA, 4},
+        .SCK  = {&GPIOA, 5},
+        .MISO = {&GPIOA, 6},
+        .MOSI = {&GPIOA, 7}
+    };
+    static const SPI_Pins SPI1_Alternate_Mapping = {
+        .NSS  = {&GPIOA, 15},
+        .SCK  = {&GPIOB, 3},
+        .MISO = {&GPIOB, 4},
+        .MOSI = {&GPIOB, 5}
+    };
 
-    spi_setupGpioHelper(outputMode, inputMode, &NSS, &SCK, &MISO, &MOSI);
+    const SPI_Pins *pinMap = (mapping == ALTERNATE) ? &SPI1_Alternate_Mapping : &SPI1_Default_Mapping;
+    spi_setupGpioHelper(outputMode, inputMode, pinMap);
 }
 
 void SPI2_SetupGpio(enum SPI_OutputMode outputMode, enum SPI_InputMode inputMode)
 {
-    static const GPIO_PortPin NSS  = {&GPIOA, 4};
-    static const GPIO_PortPin SCK  = {&GPIOA, 5};
-    static const GPIO_PortPin MISO = {&GPIOA, 6};
-    static const GPIO_PortPin MOSI = {&GPIOA, 7};
-    
-    spi_setupGpioHelper(outputMode, inputMode, &NSS, &SCK, &MISO, &MOSI);
+    static const SPI_Pins SPI2_Mapping = {
+        .NSS  = {&GPIOA, 4},
+        .SCK  = {&GPIOA, 5},
+        .MISO = {&GPIOA, 6},
+        .MOSI = {&GPIOA, 7},
+    };
+
+    spi_setupGpioHelper(outputMode, inputMode, &SPI2_Mapping);
 }
-    

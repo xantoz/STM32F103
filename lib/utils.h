5ad0b04b8202a1a225c8930d4a61d63c93464178
Anton Kindestam <antonki@kth.se>
2017-06-10 13:40:14 +0200

    BB_MAGIC

--- lib/utils.h
+++ lib/utils.h
@@ -42,10 +42,21 @@ extern void delay_us(int32_t us);
 
 #define BITBAND_SRAM_REF 0x20000000
 #define BITBAND_SRAM_BASE 0x22000000
-#define BITBAND_SRAM(a,b) ((BITBAND_SRAM_BASE + (a-BITBAND_SRAM_REF)*32 + (b*4))) // Convert SRAM address
+#define BITBAND_SRAM(a,b) ((bool*)(BITBAND_SRAM_BASE + ((a)-BITBAND_SRAM_REF)*32 + ((b)*4))) // Convert SRAM address
 #define BITBAND_PERI_REF 0x40000000
 #define BITBAND_PERI_BASE 0x42000000
-#define BITBAND_PERI(a,b) ((BITBAND_PERI_BASE + (a-BITBAND_PERI_REF)*32 + (b*4))) // Convert PERI address
+#define BITBAND_PERI(a,b) ((bool*)(BITBAND_PERI_BASE + ((a)-BITBAND_PERI_REF)*32 + ((b)*4))) // Convert PERI address
+
+#define BB_MAGIC(a,b)                               \
+    ({                                              \
+        const uint8_t *__a  = (uint8_t*)(a);        \
+        const uint8_t *ref  = __a & 0x60000000;     \
+        const uint8_t *base = ref | 0x02000000;     \
+        (bool*)(base + (__a - ref)*32 + ((b)*4));   \
+    })
+
+#define BITSET(x) ()
+#define BITRESET(x) ()
 
 #define DIV_ROUND_CLOSEST(n, d) \
     ((((n) < 0) ^ ((d) < 0)) ? (((n) - (d)/2)/(d)) : (((n) + (d)/2)/(d)))

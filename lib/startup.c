7dfeaa9e3d011f167a2ddcf6872fc10982794813
Anton Kindestam <antonki@kth.se>
2017-06-16 00:23:43 +0200

    startup: Add a boatload of new interrupt handlers to interrupt vector

--- lib/startup.c
+++ lib/startup.c
@@ -7,9 +7,6 @@
 #include "debug.h"
 #include "systick.h"
 
-void Reset_Handler(void);    // bootstrap that will call main later
-void deadend(void);          // neverending handler
-
 /* These variables are used to pass memory locations from the linker script to our code. */
 extern uint8_t _estack[];
 extern uint8_t _etext[];
@@ -19,15 +16,72 @@ extern uint8_t _edata[];
 extern uint8_t _sbss[];
 extern uint8_t _ebss[];
 
-/* we define some standard handler names here - they all default to deadend but may be changed by
- * implementing a real function with that name. So if they are triggered but undefined, we'll just
- * stop. DEFAULT_IMP defines a weak alias. */
+void deadend(void);                                         // neverending handler
+void Reset_Handler(void);
+
+// We define some standard handler names here - they all default to deadend but may be
+// changed by implementing a real function with that name. So if they are triggered but
+// undefined, we'll just stop.
 
 #define DEFAULTS_TO(func) __attribute__ ((weak, alias (#func)))
 
-void main(void);// DEFAULTS_TO(deadend);
+void main(void) DEFAULTS_TO(deadend);
+
+// Core interrupt handlers
+void NMI_Handler(void) DEFAULTS_TO(deadend);
+void HardFault_Handler(void) DEFAULTS_TO(deadend);
+void MemManage_Handler(void) DEFAULTS_TO(deadend);
+void BusFault_Handler(void) DEFAULTS_TO(deadend);
+void UsageFault_Handler(void) DEFAULTS_TO(deadend);
+void SVC_Handler(void) DEFAULTS_TO(deadend);
+void DebugMon_Handler(void) DEFAULTS_TO(deadend);
+void PendSV_Handler(void) DEFAULTS_TO(deadend);
 void Systick_Handler(void) DEFAULTS_TO(deadend);
-// TODO: define more interrupt handlers here
+
+// STM32F103_MD interrupt handlers
+void WWDG_IRQHandler(void) DEFAULTS_TO(deadend);
+void PVD_IRQHandler(void) DEFAULTS_TO(deadend);
+void TAMPER_IRQHandler(void) DEFAULTS_TO(deadend);
+void RTC_IRQHandler(void) DEFAULTS_TO(deadend);
+void FLASH_IRQHandler(void) DEFAULTS_TO(deadend);
+void RCC_IRQHandler(void) DEFAULTS_TO(deadend);
+void EXTI0_IRQHandler(void) DEFAULTS_TO(deadend);
+void EXTI1_IRQHandler(void) DEFAULTS_TO(deadend);
+void EXTI2_IRQHandler(void) DEFAULTS_TO(deadend);
+void EXTI3_IRQHandler(void) DEFAULTS_TO(deadend);
+void EXTI4_IRQHandler(void) DEFAULTS_TO(deadend);
+void DMA1_Channel1_IRQHandler(void) DEFAULTS_TO(deadend);
+void DMA1_Channel2_IRQHandler(void) DEFAULTS_TO(deadend);
+void DMA1_Channel3_IRQHandler(void) DEFAULTS_TO(deadend);
+void DMA1_Channel4_IRQHandler(void) DEFAULTS_TO(deadend);
+void DMA1_Channel5_IRQHandler(void) DEFAULTS_TO(deadend);
+void DMA1_Channel6_IRQHandler(void) DEFAULTS_TO(deadend);
+void DMA1_Channel7_IRQHandler(void) DEFAULTS_TO(deadend);
+void ADC1_2_IRQHandler(void) DEFAULTS_TO(deadend);
+void USB_HP_CAN1_TX_IRQHandler(void) DEFAULTS_TO(deadend);
+void USB_LP_CAN1_RX0_IRQHandler(void) DEFAULTS_TO(deadend);
+void CAN1_RX1_IRQHandler(void) DEFAULTS_TO(deadend);
+void CAN1_SCE_IRQHandler(void) DEFAULTS_TO(deadend);
+void EXTI9_5_IRQHandler(void) DEFAULTS_TO(deadend);
+void TIM1_BRK_IRQHandler(void) DEFAULTS_TO(deadend);
+void TIM1_UP_IRQHandler(void) DEFAULTS_TO(deadend);
+void TIM1_TRG_COM_IRQHandler(void) DEFAULTS_TO(deadend);
+void TIM1_CC_IRQHandler(void) DEFAULTS_TO(deadend);
+void TIM2_IRQHandler(void) DEFAULTS_TO(deadend);
+void TIM3_IRQHandler(void) DEFAULTS_TO(deadend);
+void TIM4_IRQHandler(void) DEFAULTS_TO(deadend);
+void I2C1_EV_IRQHandler(void) DEFAULTS_TO(deadend);
+void I2C1_ER_IRQHandler(void) DEFAULTS_TO(deadend);
+void I2C2_EV_IRQHandler(void) DEFAULTS_TO(deadend);
+void I2C2_ER_IRQHandler(void) DEFAULTS_TO(deadend);
+void SPI1_IRQHandler(void) DEFAULTS_TO(deadend);
+void SPI2_IRQHandler(void) DEFAULTS_TO(deadend);
+void USART1_IRQHandler(void) DEFAULTS_TO(deadend);
+void USART2_IRQHandler(void) DEFAULTS_TO(deadend);
+void USART3_IRQHandler(void) DEFAULTS_TO(deadend);
+void EXTI15_10_IRQHandler(void) DEFAULTS_TO(deadend);
+void RTCAlarm_IRQHandler(void) DEFAULTS_TO(deadend);
+void USBWakeUp_IRQHandler(void) DEFAULTS_TO(deadend);
 
 // TODO: use WDOG or define a default interrupt/exception handler that resets the system
 
@@ -36,90 +90,90 @@ void Systick_Handler(void) DEFAULTS_TO(deadend);
    0x0, so we put it into a separate linker section. */
 __attribute__ ((section(".isr_vector")))
 const void* vtable[] = {
-    &_estack,                //Stack top
-    Reset_Handler,           //Reset
-    deadend,                 //NMI
-    deadend,                 //Hard fault
-    deadend,                 //Memory management fault
-    deadend,                 //Bus fault
-    deadend,                 //Usage fault
-    NULL,                    //RESERVED
-    NULL,                    //RESERVED
-    NULL,                    //RESERVED
-    NULL,                    //RESERVED
-    deadend,                 //SVCall
-    deadend,                 //Reserved for Debug
-    NULL,                    //RESERVED
-    deadend,                 //PendSV handler
-    Systick_Handler,         //The SysTick handler
-    deadend,                 //IRQ0
-    deadend,                 //IRQ1
-    deadend,                 //IRQ2
-    deadend,                 //IRQ3
-    deadend,                 //IRQ4
-    deadend,                 //IRQ5
-    deadend,                 //IRQ6
-    deadend,                 //IRQ7
-    deadend,                 //IRQ8
-    deadend,                 //IRQ9
-    deadend,                 //IRQ10
-    deadend,                 //IRQ11
-    deadend,                 //IRQ12
-    deadend,                 //IRQ13
-    deadend,                 //IRQ14
-    deadend,                 //IRQ15
-    deadend,                 //IRQ16
-    deadend,                 //IRQ17
-    deadend,                 //IRQ18
-    deadend,                 //IRQ19
-    deadend,                 //IRQ20
-    deadend,                 //IRQ21
-    deadend,                 //IRQ22
-    deadend,                 //IRQ23
-    deadend,                 //IRQ24
-    deadend,                 //IRQ25
-    deadend,                 //IRQ26
-    deadend,                 //IRQ27
-    deadend,                 //IRQ28
-    deadend,                 //IRQ29
-    deadend,                 //IRQ30
-    deadend,                 //IRQ31
-    deadend,                 //IRQ32
-    deadend,                 //IRQ33
-    deadend,                 //IRQ34
-    deadend,                 //IRQ35
-    deadend,                 //IRQ36
-    deadend,                 //IRQ37
-    deadend,                 //IRQ38
-    deadend,                 //IRQ39
-    deadend,                 //IRQ40
-    deadend,                 //IRQ41
-    deadend,                 //IRQ42
-    deadend,                 //IRQ43
-    deadend,                 //IRQ44
-    deadend,                 //IRQ45
-    deadend,                 //IRQ46
-    deadend,                 //IRQ47
-    deadend,                 //IRQ48
-    deadend,                 //IRQ49
-    deadend,                 //IRQ50
-    deadend,                 //IRQ51
-    deadend,                 //IRQ52
-    deadend,                 //IRQ53
-    deadend,                 //IRQ54
-    deadend,                 //IRQ55
-    deadend,                 //IRQ56
-    deadend,                 //IRQ57
-    deadend,                 //IRQ58
-    deadend,                 //IRQ59
-    deadend,                 //IRQ60
-    deadend,                 //IRQ61
-    deadend,                 //IRQ62
-    deadend,                 //IRQ63
-    deadend,                 //IRQ64
-    deadend,                 //IRQ65
-    deadend,                 //IRQ66
-    deadend,                 //IRQ67
+    &_estack,                    // Stack top
+    Reset_Handler,               // Reset
+    NMI_Handler,                 // NMI
+    HardFault_Handler,           // Hard fault
+    MemManage_Handler,           // Memory management fault
+    BusFault_Handler,            // Bus fault
+    UsageFault_Handler,          // Usage fault
+    NULL,                        // RESERVED
+    NULL,                        // RESERVED
+    NULL,                        // RESERVED
+    NULL,                        // RESERVED
+    SVC_Handler,                 // SVCall
+    DebugMon_Handler,            // Reserved for Debug
+    NULL,                        // RESERVED
+    PendSV_Handler,              // PendSV handler
+    Systick_Handler,             // The SysTick handler
+    WWDG_IRQHandler,             // IRQ0
+    PVD_IRQHandler,              // IRQ1
+    TAMPER_IRQHandler,           // IRQ2
+    RTC_IRQHandler,              // IRQ3
+    FLASH_IRQHandler,            // IRQ4
+    RCC_IRQHandler,              // IRQ5
+    EXTI0_IRQHandler,            // IRQ6
+    EXTI1_IRQHandler,            // IRQ7
+    EXTI2_IRQHandler,            // IRQ8
+    EXTI3_IRQHandler,            // IRQ9
+    EXTI4_IRQHandler,            // IRQ10
+    DMA1_Channel1_IRQHandler,    // IRQ11
+    DMA1_Channel2_IRQHandler,    // IRQ12
+    DMA1_Channel3_IRQHandler,    // IRQ13
+    DMA1_Channel4_IRQHandler,    // IRQ14
+    DMA1_Channel5_IRQHandler,    // IRQ15
+    DMA1_Channel6_IRQHandler,    // IRQ16
+    DMA1_Channel7_IRQHandler,    // IRQ17
+    ADC1_2_IRQHandler,           // IRQ18
+    USB_HP_CAN1_TX_IRQHandler,   // IRQ19
+    USB_LP_CAN1_RX0_IRQHandler,  // IRQ20
+    CAN1_RX1_IRQHandler,         // IRQ21
+    CAN1_SCE_IRQHandler,         // IRQ22
+    EXTI9_5_IRQHandler,          // IRQ23
+    TIM1_BRK_IRQHandler,         // IRQ24
+    TIM1_UP_IRQHandler,          // IRQ25
+    TIM1_TRG_COM_IRQHandler,     // IRQ26
+    TIM1_CC_IRQHandler,          // IRQ27
+    TIM2_IRQHandler,             // IRQ28
+    TIM3_IRQHandler,             // IRQ29
+    TIM4_IRQHandler,             // IRQ30
+    I2C1_EV_IRQHandler,          // IRQ31
+    I2C1_ER_IRQHandler,          // IRQ32
+    I2C2_EV_IRQHandler,          // IRQ33
+    I2C2_ER_IRQHandler,          // IRQ34
+    SPI1_IRQHandler,             // IRQ35
+    SPI2_IRQHandler,             // IRQ36
+    USART1_IRQHandler,           // IRQ37
+    USART2_IRQHandler,           // IRQ38
+    USART3_IRQHandler,           // IRQ39
+    EXTI15_10_IRQHandler,        // IRQ40
+    RTCAlarm_IRQHandler,         // IRQ41
+    USBWakeUp_IRQHandler,        // IRQ42
+    deadend,                     // IRQ43
+    deadend,                     // IRQ44
+    deadend,                     // IRQ45
+    deadend,                     // IRQ46
+    deadend,                     // IRQ47
+    deadend,                     // IRQ48
+    deadend,                     // IRQ49
+    deadend,                     // IRQ50
+    deadend,                     // IRQ51
+    deadend,                     // IRQ52
+    deadend,                     // IRQ53
+    deadend,                     // IRQ54
+    deadend,                     // IRQ55
+    deadend,                     // IRQ56
+    deadend,                     // IRQ57
+    deadend,                     // IRQ58
+    deadend,                     // IRQ59
+    deadend,                     // IRQ60
+    deadend,                     // IRQ61
+    deadend,                     // IRQ62
+    deadend,                     // IRQ63
+    deadend,                     // IRQ64
+    deadend,                     // IRQ65
+    deadend,                     // IRQ66
+    deadend,                     // IRQ67
 };
 
 static void systemInit()

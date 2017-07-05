b3c586a8dc005c40bd5051070d225c2e2aa8e0ae
Anton Kindestam <antonki@kth.se>
2017-07-06 00:14:39 +0200

    toggleled: Toggleled now works

--- toggleled/main.c
+++ toggleled/main.c
@@ -22,11 +22,13 @@ void main(void)
     GPIO_setMODE_setCNF(&GPIOC, 13, GPIO_MODE_Output_10MHz, GPIO_Output_CNF_GPPushPull);
 
     // Interrupt EXTIO0 on falling edge of PB0
+    EXTI.IMR  |=  0x00000001; // Enable interrupt from EXTI0
+    EXTI.FTSR |=  0x00000001; // Set falling trigger select for EXTI0
+    EXTI.RTSR &= ~0x00000001; // Disable rising trigger select for EXTI0
+    NVIC_EnableInterrupt(EXTI0_IRQn);
+    AFIO_mapEXTI(0, AFIO_EXTI_PortB); // Map PB[0] to EXTI0
     GPIO_setMODE_setCNF(&GPIOB, 0, GPIO_MODE_Input, GPIO_Input_CNF_PullupPulldown);
     GPIO_setPin(&GPIOB, 0); // Pull-up
-    EXTI.IMR |= 0x00000001; // Enable interrupt from EXTI0
-    AFIO_mapEXTI(0, AFIO_EXTI_PortB); // Map PB[0] to EXTI0
-    NVIC_EnableInterrupt(EXTI0_IRQn);
 
     __enable_irq();
 
@@ -34,17 +36,23 @@ void main(void)
     while (true)
     {
         if ((cntr % 1234090) == 0)
+        {
             println_u32_hex(cntr);
+            // EXTI.SWIER = 0x00000001;
+        }
         ++cntr;
     }
 }
 
-void EXTIO0_IRQHandler(void)
+void EXTI0_IRQHandler(void)
 {
-    static bool state = 0;
+    static bool state = true;
+    print("EXTIO0\n");
     state = !state;
     if (state)
         GPIO_resetPin(&GPIOC, 13);
     else
         GPIO_setPin(&GPIOC, 13);
+
+    EXTI.PR = 0xffffffff;
 }

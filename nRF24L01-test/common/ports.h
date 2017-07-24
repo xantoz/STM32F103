c14752af2d2a457407c5e20aff28fa413c045f6d
Anton Kindestam <antonki@kth.se>
2017-07-24 10:40:35 +0200

    nRF24L01-test: forgot to commit ports.h a few commits ago...

---
+++ nRF24L01-test/common/ports.h
@@ -0,0 +1,7 @@
+#ifndef _nRF24l01_TEST_PORTS_
+#define _nRF24l01_TEST_PORTS_
+
+#define LED             ((struct GPIO_PortPin){&GPIOC, 13})
+#define DEBUG_INIT_PIN  ((struct GPIO_PortPin){&GPIOC, 15})
+
+#endif  /* _nRF24l01_TEST_PORTS_ */

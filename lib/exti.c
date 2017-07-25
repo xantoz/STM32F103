01809c5d08a6a5312f45a80f9f12fcb49dda928a
Anton Kindestam <antonki@kth.se>
2017-07-25 11:00:05 +0200

    exti: Add convenience functions for enabling/disabling + mapping EXTI interrupt using a GPIO_PortPin

---
+++ lib/exti.c
@@ -0,0 +1,87 @@
+#include "exti.h"
+
+#include "afio.h"
+#include "nvic.h"
+#include "IRQn.h"
+#include "debug.h"
+
+static enum AFIO_EXTICRx_Flg EXTI_GPIOxToAFIOPort(volatile const struct GPIO_Port * const port)
+{
+    if (port == &GPIOA)
+        return AFIO_EXTI_PortA;
+    else if (port == &GPIOB)
+        return AFIO_EXTI_PortB;
+    else if (port ==  &GPIOC)
+        return AFIO_EXTI_PortC;
+
+    die("no such port");
+}
+
+static enum IRQn EXTI_pinNrToIRQn(uint8_t pin)
+{
+    if (pin <= 4)
+    {
+        return EXTI0_IRQn + pin;
+    }
+    else if (5 <= pin && pin <= 9)
+    {
+        return EXTI9_5_IRQn;
+    }
+    else if (9 <= pin && pin <= 15)
+    {
+        return EXTI15_10_IRQn;
+    }
+
+    die("pin out of range");
+}
+
+void EXTI_enableInterrupt(const struct GPIO_PortPin * const portpin, enum EXTI_Flank flank)
+{
+    assert(IS_GPIO(*portpin->port));
+    assert(portpin->pin <= 15);
+
+    irq_lock_t lock;
+    LOCK_IRQ(lock);
+
+    const uint32_t bit = 0x1 << portpin->pin;
+    switch (flank)
+    {
+        case EXTI_NONE:
+            EXTI.FTSR &= ~bit; // Disable falling trigger select
+            EXTI.RTSR &= ~bit; // Disable rising trigger select
+            break;
+        case EXTI_RISING:
+            EXTI.FTSR &= ~bit; // Disable falling trigger select
+            EXTI.RTSR |=  bit; // Enable rising trigger select
+            break;
+        case EXTI_FALLING:
+            EXTI.FTSR |=  bit; // Enable falling trigger select
+            EXTI.RTSR &= ~bit; // Disable rising trigger select
+            break;
+        case EXTI_BOTH:
+            EXTI.FTSR |=  bit; // Enable falling trigger select
+            EXTI.RTSR |=  bit; // Enable rising trigger select
+            break;
+    }
+    AFIO_mapEXTI(portpin->pin, EXTI_GPIOxToAFIOPort(portpin->port)); // Map port
+    EXTI.PR = bit;                                          // Clear bit in pending register
+    NVIC_EnableInterrupt(EXTI_pinNrToIRQn(portpin->pin));
+    EXTI.IMR |= bit;                                        // Enable interrupt for EXTIx
+
+    UNLOCK_IRQ(lock);
+}
+
+void EXTI_disableInterrupt(const struct GPIO_PortPin * const portpin)
+{
+    assert(IS_GPIO(*portpin->port));
+    assert(portpin->pin <= 15);
+
+    irq_lock_t lock;
+    LOCK_IRQ(lock);
+
+    const uint32_t bit = 0x1 << portpin->pin;
+    NVIC_DisableInterrupt(EXTI_pinNrToIRQn(portpin->pin));
+    EXTI.IMR &= ~bit;
+
+    UNLOCK_IRQ(lock);
+}
--- lib/exti.h
+++ lib/exti.h
@@ -7,6 +7,9 @@
 #ifndef _EXTI_
 #define _EXTI_
 
+#include "types.h"
+#include "gpio.h"
+
 /**
  * @brief EXTI register definitions
  */
@@ -21,5 +24,31 @@ struct EXTI_Regs
 };
 
 extern volatile struct EXTI_Regs EXTI;
+
+/**
+ * @brief EXTI flank trigger selector
+ */
+enum EXTI_Flank
+{
+    EXTI_NONE,    //!< Trigger on no flank (effectively disabling, useful for SW-triggered interrupts)
+    EXTI_RISING,  //!< Trigger on rising flank
+    EXTI_FALLING, //!< Trigger on falling flank
+    EXTI_BOTH,    //!< Trigger on both flanks
+};
+
+/**
+ * @brief Enable EXTI IRQ for `portpin` on flank `flank`
+ *
+ * @note  GPIO setup needs to be done separately
+ * @note  Pins 0-4 have an interrupt handler each
+ * @note  Pins 5-9 share the same interrupt handler, you will have to inspect EXTI.PR
+ * @note  Pins 10-15 share the same interrupt handler, you will have to inspect EXTI.PR
+ */
+void EXTI_enableInterrupt(const struct GPIO_PortPin * const portpin, enum EXTI_Flank flank);
+
+/**
+ * @brief Disable EXTI IRQ for `portpin`
+ */
+void EXTI_disableInterrupt(const struct GPIO_PortPin * const portpin);
 
 #endif
